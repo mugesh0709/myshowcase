610ea00513b709462d8649da43e8117d12b28def
mugesh <151634675+mugesh0709@users.noreply.github.com>
2024-04-24 11:15:52 +0530

    Add files via upload

---
+++ portfolios/script.js
@@ -0,0 +1,23 @@
+ScrollReveal().reveal('.navbar', { delay: 500 ,distance: '80px' ,duration: 800 ,easing: 'ease-in', origin: 'top'});
+
+ScrollReveal().reveal('.home-top .home-text', { delay: 1200 ,distance: '120px' ,duration: 1000 ,easing: 'ease-in', origin: 'left' ,scale: 0.85 ,interval: 600 });
+
+ScrollReveal().reveal('.home-top .home-img', { delay: 1900 ,distance: '120px' ,duration: 1000 ,easing: 'ease-out', origin: 'right' ,scale: 0.85 ,interval: 600 });
+
+ScrollReveal().reveal('.about-img', { delay: 1200 ,distance: '120px' ,duration: 1000 ,easing: 'ease-out', origin: 'right' ,rotate: { x: 10, z: 40} ,interval: 600 });
+
+ScrollReveal().reveal('.about-box', { delay: 1000 ,distance: '120px' ,duration: 800 ,easing: 'ease-out', origin: 'left' ,rotate: { x: 10, y: 40, z:20} ,interval: 600 });
+
+ScrollReveal().reveal('.about-text .para', { delay: 800 ,distance: '120px' ,duration: 600 ,easing: 'ease-out', origin: 'right' ,interval: 600 });
+
+ScrollReveal().reveal('.about-text .btn', { delay: 1700 ,distance: '120px' ,duration: 1000 ,easing: 'ease-out', origin: 'right' ,interval: 600 });
+
+ScrollReveal().reveal('.about-text .progressing .progress', { delay: 200 ,distance: '60px' ,duration: 400 ,easing: 'steps(1)', origin: 'top' ,interval: 300 });
+
+ScrollReveal().reveal('.btn-group', { delay: 2000 ,distance: '120px' ,duration: 1200 ,easing: 'ease-out', origin: 'top',interval: 900 ,scale: 0.85});
+
+ScrollReveal().reveal('.page1 .card', { delay: 900 ,distance: '120px' ,duration: 1000 ,easing: 'steps(1)', origin: 'left',interval: 600 ,scale: 0.85});
+
+ScrollReveal().reveal('.contact-us', { delay: 800 ,distance: '120px' ,duration: 1000 ,easing: 'ease-out', origin: 'left',interval: 900});
+
+ScrollReveal().reveal('.contact-info ', { delay: 1200 ,distance: '120px' ,duration: 1000 ,easing: 'ease-out', origin: 'right',interval: 900});
